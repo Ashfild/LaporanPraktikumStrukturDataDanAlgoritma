e321c4577cd075903992580a61d867d40dced375
Fadhil <88092387+Ashfild@users.noreply.github.com>
2024-06-12 07:39:00 +0700

    Update unguided2.cpp

--- modul9/unguided2.cpp
+++ modul9/unguided2.cpp
@@ -3,33 +3,33 @@
 
 using namespace std;
 
-void inputNamaSimpul(vector<string> &nama_simpul, int jumlah_simpul) {
+void inputNamaSimpul(vector<string> &nama_simpul, int jumlah_simpul_2311102032) {
     cout << "Silahkan masukkan nama simpul:" << endl;
-    for (int i = 0; i < jumlah_simpul; ++i) {
+    for (int i = 0; i < jumlah_simpul_2311102032; ++i) {
         cout << "Simpul " << i + 1 << ": ";
         cin >> nama_simpul[i];
     }
 }
 
-void inputBobot(vector<vector<int>> &bobot, const vector<string> &nama_simpul, int jumlah_simpul) {
+void inputBobot(vector<vector<int>> &bobot, const vector<string> &nama_simpul, int jumlah_simpul_2311102032) {
     cout << "Silahkan masukkan bobot antar simpul:" << endl;
-    for (int i = 0; i < jumlah_simpul; ++i) {
-        for (int j = 0; j < jumlah_simpul; ++j) {
+    for (int i = 0; i < jumlah_simpul_2311102032; ++i) {
+        for (int j = 0; j < jumlah_simpul_2311102032; ++j) {
             cout << nama_simpul[i] << "-->" << nama_simpul[j] << " = ";
             cin >> bobot[i][j];
         }
     }
 }
 
-void tampilkanTabelJarak(const vector<string> &nama_simpul, const vector<vector<int>> &bobot, int jumlah_simpul) {
+void tampilkanTabelJarak(const vector<string> &nama_simpul, const vector<vector<int>> &bobot, int jumlah_simpul_2311102032) {
     cout << "\t";
-    for (int i = 0; i < jumlah_simpul; ++i) {
+    for (int i = 0; i < jumlah_simpul_2311102032; ++i) {
         cout << nama_simpul[i] << "\t";
     }
     cout << endl;
-    for (int i = 0; i < jumlah_simpul; ++i) {
+    for (int i = 0; i < jumlah_simpul_2311102032; ++i) {
         cout << nama_simpul[i] << "\t";
-        for (int j = 0; j < jumlah_simpul; ++j) {
+        for (int j = 0; j < jumlah_simpul_2311102032; ++j) {
             cout << bobot[i][j] << "\t";
         }
         cout << endl;
@@ -37,7 +37,7 @@ void tampilkanTabelJarak(const vector<string> &nama_simpul, const vector<vector<
 }
 
 int main() {
-    int jumlah_simpul;
+    int jumlah_simpul_2311102032;
     vector<string> nama_simpul;
     vector<vector<int>> bobot;
 
@@ -56,33 +56,33 @@ int main() {
         switch (pilihan) {
             case 1:
                 cout << "Silahkan masukkan jumlah simpul: ";
-                cin >> jumlah_simpul;
-                nama_simpul.resize(jumlah_simpul);
-                bobot.resize(jumlah_simpul, vector<int>(jumlah_simpul));
+                cin >> jumlah_simpul_2311102032;
+                nama_simpul.resize(jumlah_simpul_2311102032);
+                bobot.resize(jumlah_simpul_2311102032, vector<int>(jumlah_simpul_2311102032));
                 break;
             case 2:
-                if (jumlah_simpul == 0) {
+                if (jumlah_simpul_2311102032 == 0) {
                     cout << "Silahkan input jumlah simpul terlebih dahulu!\n";
                 } else {
-                    inputNamaSimpul(nama_simpul, jumlah_simpul);
+                    inputNamaSimpul(nama_simpul, jumlah_simpul_2311102032);
                 }
                 break;
             case 3:
-                if (jumlah_simpul == 0) {
+                if (jumlah_simpul_2311102032 == 0) {
                     cout << "Silahkan input jumlah simpul terlebih dahulu!\n";
                 } else {
-                    inputBobot(bobot, nama_simpul, jumlah_simpul);
+                    inputBobot(bobot, nama_simpul, jumlah_simpul_2311102032);
                 }
                 break;
             case 4:
-                if (jumlah_simpul == 0) {
+                if (jumlah_simpul_2311102032 == 0) {
                     cout << "Silahkan input jumlah simpul terlebih dahulu!\n";
                 } else if (nama_simpul.empty()) {
                     cout << "Silahkan input nama simpul terlebih dahulu!\n";
                 } else if (bobot.empty()) {
                     cout << "Silahkan input bobot antar simpul terlebih dahulu!\n";
                 } else {
-                    tampilkanTabelJarak(nama_simpul, bobot, jumlah_simpul);
+                    tampilkanTabelJarak(nama_simpul, bobot, jumlah_simpul_2311102032);
                 }
                 break;
             case 5:
