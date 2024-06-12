c33d5c5530ee88f3a466798efd72cc9d0d7f3cca
Fadhil <88092387+Ashfild@users.noreply.github.com>
2024-06-12 07:37:56 +0700

    Update unguided1.cpp

--- modul9/unguided1.cpp
+++ modul9/unguided1.cpp
@@ -4,22 +4,22 @@
 using namespace std;
 
 int main() {
-    int jumlah_simpul;
+    int jumlah_simpul_2311102032;
     cout << "Silahkan masukkan jumlah simpul: ";
-    cin >> jumlah_simpul;
+    cin >> jumlah_simpul_2311102032;
 
-    vector<string> nama_simpul(jumlah_simpul);
-    vector<vector<int>> bobot(jumlah_simpul, vector<int>(jumlah_simpul));
+    vector<string> nama_simpul(jumlah_simpul_2311102032);
+    vector<vector<int>> bobot(jumlah_simpul_2311102032, vector<int>(jumlah_simpul_2311102032));
 
     cout << "Silahkan masukkan nama simpul:" << endl;
-    for (int i = 0; i < jumlah_simpul; ++i) {
+    for (int i = 0; i < jumlah_simpul_2311102032; ++i) {
         cout << "Simpul " << i + 1 << ": ";
         cin >> nama_simpul[i];
     }
 
     cout << "Silahkan masukkan bobot antar simpul:" << endl;
-    for (int i = 0; i < jumlah_simpul; ++i) {
-        for (int j = 0; j < jumlah_simpul; ++j) {
+    for (int i = 0; i < jumlah_simpul_2311102032; ++i) {
+        for (int j = 0; j < jumlah_simpul_2311102032; ++j) {
             cout << nama_simpul[i] << "-->" << nama_simpul[j] << " = ";
             cin >> bobot[i][j];
         }
@@ -27,13 +27,13 @@ int main() {
 
     // Output tabel jarak
     cout << "\t";
-    for (int i = 0; i < jumlah_simpul; ++i) {
+    for (int i = 0; i < jumlah_simpul_2311102032; ++i) {
         cout << nama_simpul[i] << "\t";
     }
     cout << endl;
-    for (int i = 0; i < jumlah_simpul; ++i) {
+    for (int i = 0; i < jumlah_simpul_2311102032; ++i) {
         cout << nama_simpul[i] << "\t";
-        for (int j = 0; j < jumlah_simpul; ++j) {
+        for (int j = 0; j < jumlah_simpul_2311102032; ++j) {
             cout << bobot[i][j] << "\t";
         }
         cout << endl;
